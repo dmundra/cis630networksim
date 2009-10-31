2a6caa125c1a13e2f8b03389c362d2bceec60740
danielmundra <danielmundra@6b987052-bf7b-11de-8ec6-755e00613368>
2009-10-31 23:11:09 +0000

    Updated Kernel Implementation with RIP algorithm thats is simple right now, needs to be cleaned up. Added javadocs.

--- trunk/src/network/impl/kernel/KernelImpl.java
+++ trunk/src/network/impl/kernel/KernelImpl.java
@@ -1,66 +1,84 @@
 package network.impl.kernel;
 
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import network.AbstractKernel;
 import network.Interface;
-import network.Kernel;
 import network.KnownPort;
 import network.Message;
 import network.Interface.DisconnectedException;
 
-public class KernelImpl implements Kernel {
+public class KernelImpl extends AbstractKernel {
 
 	private CopyOnWriteArrayList<Interface> interfaceList = new CopyOnWriteArrayList<Interface>();
 	private HashMap<Integer, Node> routingTable = new HashMap<Integer, Node>();
-	private int address = -1;
 	
 	@Override
 	public void interfaceAdded(Interface iface) {
 		interfaceList.add(iface);		
 	}
-
+	
 	@Override
 	public void interfaceConnected(Interface iface) {
-		// TODO
+		// TODO Auto-generated method stub
+		
 	}
-
 	@Override
 	public void interfaceDisconnected(Interface iface) {
 		interfaceList.remove(iface);		
 	}
-
+	
 	@Override
 	public void shutDown() {
+		// TODO Auto-generated method stub
 		
 	}
-
+	
 	@Override
 	public void start() {
-		for(Interface i : interfaceList) {
-			try {				
-				Message<Node> findNeighbors = new Message<Node>(address, -1, KnownPort.KERNEL_WHO.ordinal(), KnownPort.KERNEL_WHO.ordinal(), new Node(address));
-				i.send(findNeighbors);
-			} catch (DisconnectedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		while(true) {
+			// Runs the RIP algorithm every 50 seconds for now.
+			Timer checkNeighbors = new Timer("checkNeighbors");			
+			checkNeighbors.schedule(new RIPTask(), 50000);			
 		}
 	}
-
-	@Override
-	public void setAddress(int address) {
-		// TODO Auto-generated method stub
-		
+	
+	/**
+	 * Class that runs the RIP algorithm to find all neighbors and add them to Kernel's
+	 * routing table.
+	 */
+	class RIPTask extends TimerTask {		
+		// TODO Clean up and add RIP algorithm
+		public void run() {
+			for(Interface i : interfaceList) {
+				try {
+					Node kernelNode = new Node(getAddress());
+					Message<Node> findNeighbors = new Message<Node>(getAddress(), 255, KnownPort.KERNEL_WHO.ordinal(), KnownPort.KERNEL_WHO.ordinal(), kernelNode);
+					i.send(findNeighbors);				
+					
+				} catch (DisconnectedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			for(Interface i : interfaceList) {
+				try {
+					Message<Node> recievedMessage = i.receive().asType(Node.class);
+					
+					routingTable.put(recievedMessage.source, recievedMessage.data);
+					
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}			
+		}		
 	}
-
-	@Override
-	public void setName(String name) {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
