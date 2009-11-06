dcb8d13c5b29df20fad819ec4a6e36b3e8e9bf0c
awittig82 <awittig82@6b987052-bf7b-11de-8ec6-755e00613368>
2009-11-06 19:25:44 +0000

    added in what I think is a bad rip algorithm (might be n^n). To fix I think that we'll have to keep track of the messages that never make it back and remove them from the table instead of starting from scratch each round. Anyway, shouldn't be too bad to fix.

--- trunk/src/network/impl/kernel/KernelImpl.java
+++ trunk/src/network/impl/kernel/KernelImpl.java
@@ -1,9 +1,9 @@
 package network.impl.kernel;
 
-import java.util.HashMap;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.ConcurrentHashMap;
 
 import network.AbstractKernel;
 import network.Interface;
@@ -13,12 +13,22 @@ import network.Interface.DisconnectedException;
 
 public class KernelImpl extends AbstractKernel {
 
-	private CopyOnWriteArrayList<Interface> interfaceList = new CopyOnWriteArrayList<Interface>();
-	private HashMap<Integer, Node> routingTable = new HashMap<Integer, Node>();
+	/**this is a map of our interface to unique integer*/
+	private ConcurrentHashMap<Interface, Integer> interfaceList = new ConcurrentHashMap<Interface, Integer>();
+	private ConcurrentHashMap<Integer, Node> routingTable = new ConcurrentHashMap<Integer, Node>();
 	
 	@Override
 	public void interfaceAdded(Interface iface) {
-		interfaceList.add(iface);		
+		
+		int i = interfaceList.size() - 1;
+		synchronized(interfaceList){
+			for(Integer j : interfaceList.values()){
+				i = Math.max(i, j);
+			}
+			
+			//we should have a unique i
+			interfaceList.put(iface, i);	
+		}
 	}
 	
 	@Override
@@ -28,7 +38,11 @@ public class KernelImpl extends AbstractKernel {
 	}
 	@Override
 	public void interfaceDisconnected(Interface iface) {
-		interfaceList.remove(iface);		
+		
+		//want to sync so that we don't remove while adding
+		synchronized(interfaceList){
+			interfaceList.remove(iface);		
+		}
 	}
 	
 	@Override
@@ -42,7 +56,7 @@ public class KernelImpl extends AbstractKernel {
 		while(true) {
 			// Runs the RIP algorithm every 50 seconds for now.
 			Timer checkNeighbors = new Timer("checkNeighbors");			
-			checkNeighbors.schedule(new RIPTask(), 50000);			
+			checkNeighbors.schedule(new RIPTask(), 10000);			
 		}
 	}
 	
@@ -51,9 +65,9 @@ public class KernelImpl extends AbstractKernel {
 	 * routing table.
 	 */
 	class RIPTask extends TimerTask {		
-		// TODO Clean up and add RIP algorithm
+		// TODO Clean up and fix RIP algorithm (current implementation of rip might be n^n...)
 		public void run() {
-			for(Interface i : interfaceList) {
+			for(Interface i : interfaceList.keySet()) {
 				try {
 					Node kernelNode = new Node(address());
 					Message<Node> findNeighbors = new Message<Node>(address(), 255, KnownPort.KERNEL_WHO.ordinal(), KnownPort.KERNEL_WHO.ordinal(), kernelNode);
@@ -68,17 +82,58 @@ public class KernelImpl extends AbstractKernel {
 				}
 			}
 			
-			for(Interface i : interfaceList) {
+			//we want to make a new routing table that will replace our current when we are done:
+			ConcurrentHashMap<Integer, Node> routingTableNew = new ConcurrentHashMap<Integer, Node>();
+			
+			for(Map.Entry<Interface, Integer> pair : interfaceList.entrySet()) {
 				try {
-					Message<Node> recievedMessage = i.receive().asType(Node.class);
-					
-					routingTable.put(recievedMessage.source, recievedMessage.data);
+					//TODO: do our normal work if it isn't a rip message...
+					Message<Node> recievedMessage = pair.getKey().receive().asType(Node.class);
+					Node neighbor = recievedMessage.data.clone();
+					neighbor.setCost(1);
+					neighbor.setLink(pair.getValue());
+					routingTableNew.put(recievedMessage.source, neighbor);
 					
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			}			
+			}
+			
+			
+			//now let's fix up our routing table:
+			for(boolean madeChanges = true; madeChanges; ){
+				madeChanges = false;
+				
+				for(Node n : routingTableNew.values()){
+					//see if the node has a routing table 
+					for(Map.Entry<Integer, Node> pair : n.getRoutingTable().entrySet()){
+						Node toAdd = pair.getValue().clone();
+						toAdd.setCost(toAdd.getCost() + 1);
+						//see if the address is in our table:
+						if(routingTableNew.containsKey(pair.getKey())){
+							//the address is already here, so see if we are better:
+							if(routingTableNew.get(pair.getKey()).getCost() > toAdd.getCost()){
+								//looks like we have a better way to get there:
+								routingTableNew.replace(pair.getKey(), toAdd);
+								//don't think we should set madeChanges = true;
+							}
+						}else {
+							//see if the address is not us:
+							if(address() != pair.getKey()){
+								//this might throw exception at runtime... I think that concurrentHashMap can do this though
+								routingTableNew.put(pair.getKey(), toAdd);
+								madeChanges = true;
+							}
+						}
+					}
+				}
+			}
+			
+			//now start using our new routing table:
+			routingTable = routingTableNew;
+			
+			
 		}		
 	}
 }
--- trunk/src/network/impl/kernel/Node.java
+++ trunk/src/network/impl/kernel/Node.java
@@ -1,14 +1,23 @@
 package network.impl.kernel;
 
 import java.io.Serializable;
+import java.util.HashMap;
 
 /**
  * Each Kernel has a Node which contains the address data, no of hops, link.
- * @author Anthony Wittig
  */
 public class Node implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+	private HashMap<Integer, Node> routingTable = new HashMap<Integer, Node>(); 
+
+	public HashMap<Integer, Node> getRoutingTable() {
+		return routingTable;
+	}
+
+	public void setRoutingTable(HashMap<Integer, Node> routingTable) {
+		this.routingTable = routingTable;
+	}
 
 	/**
 	 * The address that this entry is all about
@@ -31,6 +40,22 @@ public class Node implements Serializable {
 	 */
 	public Node(int address) {
 		this.address = address;
+		this.cost = 0;
+		this.link = -1;
+	}
+	
+	/**
+	 * returns a shallow copy (routing table is same a cloner)
+	 * 
+	 * @return a shallow copy
+	 */
+	@Override
+	public Node clone(){
+		Node clone = new Node(address);
+		clone.cost = cost;
+		clone.link = link;
+		clone.routingTable = routingTable;
+		return clone;
 	}
 
 	public int getCost() {
