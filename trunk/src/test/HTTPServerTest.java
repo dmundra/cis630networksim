b5ee1d9f84160608b33704708f1d86e747bc7e7a
luke.v.maurer <luke.v.maurer@6b987052-bf7b-11de-8ec6-755e00613368>
2009-11-29 02:26:28 +0000

    Added an HTTP server implementation, with one simple test.

---
+++ trunk/src/test/HTTPServerTest.java
@@ -0,0 +1,48 @@
+package test;
+
+import network.Interface;
+import network.Message;
+import network.Node;
+import network.Simulator;
+import network.SimulatorFactory;
+import network.protocols.HTTP;
+import network.software.HTTPServer;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HTTPServerTest extends AbstractTest {
+    private static final String LOCATION = "/index.html", CONTENT = "Hello world!";
+    
+    @Test
+    public void test() throws Exception {
+        final Simulator sim = SimulatorFactory.instance().createSimulator();
+        
+        final HTTPServer service = new HTTPServer();
+        
+        final Node server = sim.buildNode(1)
+            .name("Server")
+            .kernel(sim.createUserKernel(service))
+            .create();
+        
+        final Node client = sim.buildNode(2)
+            .name("Client")
+            .connections(server)
+            .create();
+        
+        service.addFile(LOCATION, CONTENT);
+        
+        sim.start();
+        
+        final Interface iface = client.interfaces().get(0);
+        
+        iface.send(new Message<HTTP.Request>(1, 2, -1, HTTP.PORT.number(),
+                new HTTP.Request(HTTP.Request.Method.GET, LOCATION)));
+        
+        final HTTP.Response response =
+            iface.receive().dataAs(HTTP.Response.class);
+        
+        Assert.assertEquals(response.status, HTTP.Response.Status.OK);
+        Assert.assertEquals(response.content, CONTENT);
+    }
+}
