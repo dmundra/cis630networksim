f0e07ea6006fc31b0a66295cec79cd25b0075492
awittig82 <awittig82@6b987052-bf7b-11de-8ec6-755e00613368>
2009-12-06 04:28:52 +0000

    working on bully, what a mess!

--- trunk/src/test/BullyTest.java
+++ trunk/src/test/BullyTest.java
@@ -19,8 +19,9 @@ import org.testng.annotations.Test;
 public class BullyTest extends AbstractTest {
 
 	private static final long TIMEOUT = 0;
-	private static final long WAIT_FOR_LEADER_RESPONSE = 16;
+	private static final long WAIT_FOR_LEADER_RESPONSE = 10;
 	private static final long WAIT_FOR_LEADER_RESPONSE_TO_ELECTION = WAIT_FOR_LEADER_RESPONSE * 2;
+	private static final long WAIT_FOR_NEW_LEADER_RESPONSE_TO_ELECTION = WAIT_FOR_LEADER_RESPONSE_TO_ELECTION * 2;
 
 	private static final Random rand = new Random();
 
@@ -97,7 +98,7 @@ public class BullyTest extends AbstractTest {
 		private BTuple sendElectionP1(Message<ETuple> electionMessage)
 				throws InterruptedException {
 
-			os().logger().info("Election has started!");
+			os().logger().info("Election called, start msg is null: " + (electionMessage == null));
 
 			if (electionMessage == null) {
 				// we must be starting an election, not responding to an
@@ -131,9 +132,12 @@ public class BullyTest extends AbstractTest {
 			} else {
 				// check to see if we need to update our election clock:
 				if (electionMessage.data.electionNum >= electionNum) {
+					os().logger().info("my election num: " + electionNum + ", new election num: " + electionMessage.data.electionNum);
 					electionNum = electionMessage.data.electionNum;
+					
 				} else {
 					// this means that it's old:
+					os().logger().info("this is an old election, ignore election process");
 					return BTuple.get(false, false);
 				}
 			}
@@ -180,7 +184,7 @@ public class BullyTest extends AbstractTest {
 			}
 
 			try {
-				while (true) {
+				while (meLead) {
 					Message<ETuple> message = os().receive(ELECTION_PORT,
 							WAIT_FOR_LEADER_RESPONSE_TO_ELECTION,
 							TimeUnit.SECONDS).asType(ETuple.class);
@@ -189,17 +193,31 @@ public class BullyTest extends AbstractTest {
 					if (message.data.electionNum > electionNum) {
 						// we have been working on an old election:
 						// let's go with the new
+						os().logger().info(
+								"we've been working on an old election "
+										+ message.data.address);
 						return sendElectionP1(message);
 					} else if (message.data.electionNum < electionNum) {
 						// we can ignore this message
+						os().logger().info(
+								"we got an old message, ignoring it "
+										+ message.data.address);
 					}else {
 						if (message.data.address > os().address()) {
 							// respond with the "I've voted" message
 							os().send(message.source, ELECTION_PORT, ELECTION_PORT,
-									os().address());
+									ETuple.get(os().address(), electionNum));
+							
+							os().logger().info(
+									"responding to a inferior chaps request to vote: "
+											+ message.data.address);
 						} else {
 							// we know that we aren't the leader, someone else is
 							meLead = false;
+							
+							os().logger().info(
+									"someone higher than I responded: "
+											+ message.data.address);
 						}
 					}
 
@@ -214,6 +232,7 @@ public class BullyTest extends AbstractTest {
 
 		public boolean sendElectionP2(boolean meLead)
 				throws InterruptedException {
+			
 			if (meLead) {
 				os().logger()
 						.info("I won! I am the leader - " + os().address());
@@ -221,37 +240,47 @@ public class BullyTest extends AbstractTest {
 				// we win!
 				for (int j = 0; j < members.size(); ++j) {
 					Integer i = members.get(j);
-					// if (i < os().address()) {
+
 					try {
 						os().send(i, RESULT_PORT, RESULT_PORT, ETuple.get(os().address(), electionNum));
 					} catch (DisconnectedException e) {
 						os().logger().info("Oops. I've been disconnected.");
 					}
-					// }
+					
 				}
 			} else {
 				// someone else should have won,
 				try {
 					Message<ETuple> message = os().receive(RESULT_PORT,
-							WAIT_FOR_LEADER_RESPONSE_TO_ELECTION,
+							WAIT_FOR_NEW_LEADER_RESPONSE_TO_ELECTION,
 							TimeUnit.SECONDS).asType(ETuple.class);
 
 					os().logger().info(
 							"I am not the leader, but this guys says he is - "
 									+ message.data.address);
+					
 					leader = message.data.address;
 
-				} catch (InterruptedException e) {
+				} catch (NullPointerException e) {
 					// we didn't get a response, call another election?
 					return false;
 				}
 			}
+			
 			return true;
 		}
 
 		protected void run() throws InterruptedException {
 
 			Thread.sleep(8000);
+			
+			//remove ourselves from our list:
+			for(int i = 0; i < members.size(); ++i){
+				if(members.get(i) == os().address()){
+					members.remove(i);
+					break;
+				}
+			}
 
 			while (true) {
 
@@ -266,14 +295,18 @@ public class BullyTest extends AbstractTest {
 				// they won, in either case, we need to do an election:
 				try {
 
-					electionMessage = os().receive(ELECTION_PORT, 1,
-							TimeUnit.MILLISECONDS).asType(ETuple.class);
-					
-					if(electionMessage.data.electionNum >= electionNum){
-						// call an election
-						callElection = true;
+					//clear off our buffer until we see that we need an election:
+					while(!callElection){
+						
+						electionMessage = os().receive(ELECTION_PORT, 1,
+								TimeUnit.MILLISECONDS).asType(ETuple.class);
+						
+						if(electionMessage.data.electionNum >= electionNum){
+							// call an election
+							callElection = true;
+						}
 					}
-				} catch (Exception e) {
+				} catch (NullPointerException e) {
 					// good, let's keep going
 				}
 
@@ -305,18 +338,22 @@ public class BullyTest extends AbstractTest {
 							callElection = true;
 						}
 					}
-				} catch (Exception e) {
+				} catch (NullPointerException e) {
 				}
 
 				if (callElection || !sendToLeader()) {
-					os().logger().info("Election code from run()");
+					os().logger().info("Election code from run() electionMessage is null: " + (electionMessage == null));
 
 					while (true) {
 						BTuple ret = sendElectionP1(electionMessage);
 						if (ret.b1) {
 							if (sendElectionP2(ret.b2)) {
 								break;
+							}else{
+								os().logger().info("didn't make it out of p2");
 							}
+						}else{
+							break;
 						}
 					}
 				}
@@ -333,18 +370,18 @@ public class BullyTest extends AbstractTest {
 		final ArrayList<Integer> members = new ArrayList<Integer>();
 		members.add(1);
 		members.add(2);
-		members.add(3);
+		//members.add(3);
 //		members.add(4);
 //		members.add(5);
 
 		final Node a = sim.buildNode(1).name("A").kernel(
-				sim.createUserKernel(new BullyProcess(members, 1))).create();
+				sim.createUserKernel(new BullyProcess((ArrayList<Integer>)members.clone(), 0))).create();
 
 		final Node b = sim.buildNode(2).name("B").kernel(
-				sim.createUserKernel(new BullyProcess(members, 1))).create();
+				sim.createUserKernel(new BullyProcess((ArrayList<Integer>)members.clone(), 0))).create();
 
-		final Node c = sim.buildNode(3).name("C").kernel(
-				sim.createUserKernel(new BullyProcess(members, 1))).create();
+//		final Node c = sim.buildNode(3).name("C").kernel(
+//				sim.createUserKernel(new BullyProcess((ArrayList<Integer>)members.clone(), 0))).create();
 
 		// final Node d = sim.buildNode(4).name("D").kernel(
 		// sim.createUserKernel(new BullyProcess(members, 1))).create();
@@ -352,8 +389,7 @@ public class BullyTest extends AbstractTest {
 		// final Node e = sim.buildNode(5).name("E").kernel(
 		// sim.createUserKernel(new BullyProcess(members, 1))).create();
 
-		final Node router1 = sim.buildNode(6).name("Router1").connections(a, b,
-				c).create();
+		final Node router1 = sim.buildNode(6).name("Router1").connections(a, b).create();
 
 		// final Node router2 = sim.buildNode(7).name("Router2").connections(
 		// router1, d, e).create();
@@ -361,7 +397,7 @@ public class BullyTest extends AbstractTest {
 		sim.start();
 
 		Thread.sleep(10000);
-		sim.disconnect(router1.interfaces().get(0));
+		//sim.disconnect(router1.interfaces().get(0));
 
 		Thread.sleep(Long.MAX_VALUE);
 
